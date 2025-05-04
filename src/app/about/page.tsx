ad8345286f5d1296189de26f2fd52b623cdad4ba
haradayuuta <mayutokuyuta@gmail.com>
2025-05-04 17:20:10 +0900

    ♻️ ヘッドライン部分をコンポーネント化

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import { Footer } from "../../../components/Footer";
 import { Links } from "../../../components/Links"
+import Link from "next/link";
+import { Headline } from "../../../components/Headline";
  
 export default function Home() {
   return (
@@ -14,19 +16,7 @@ export default function Home() {
           height={38}
           priority
         />
-        <h1>about/page.js</h1>
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+        <Headline title="About Page!!!!" page="about"/>
 
         <Links />
       </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import { Footer } from "../../components/Footer";
 import { Links } from "../../components/Links";
+import { Headline } from "../../components/Headline";
 
 export default function Home() {
   return (
@@ -14,21 +15,8 @@ export default function Home() {
           height={38}
           priority
         />
-        <h1>page.js</h1>
-
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
+        
+        <Headline title="Index Page" page="index"/>
         <Links />
       </main>
 
