cc69c0aa425c3c79f1f020ce4c672d9f01cceb03
haradayuuta <mayutokuyuta@gmail.com>
2025-05-05 11:22:17 +0900

    ♻️ Main部分のコンポーネントカ

--- components/Headline.js
+++ components/Headline.js
@@ -4,8 +4,7 @@ export function Headline(props) {
     console.log(props);
     return (
         <div>
-            <h1>{props.title}</h1>
-
+            <h1>{props.page} Page</h1>
             <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
                 <li className="mb-2 tracking-[-.01em]">
                     Get started by editing{" "}
--- components/Main.js
+++ components/Main.js
@@ -2,24 +2,14 @@ import Image from "next/image";
 import { Links } from "./Links";
 import { Headline } from "./Headline";
 
-export function Main() {
+export function Main(props) {
   return (
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-
         <Headline
-          title="Index Page"
-          page="index"
+          page={props.page}
         >
           code={<code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-            src/app/index.tsx
+            src/app/{props.page}.tsx
           </code>}
         </Headline>
         <Links />
