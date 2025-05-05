69b7e4a52cae496297d09bd0ffbcbb37a9ae738e
haradayuuta <mayutokuyuta@gmail.com>
2025-05-05 12:17:29 +0900

    ✨ Headerの追加

---
+++ components/Header.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+import classes from "./Header.module.css"
+
+export function Header() {
+    return (
+
+        <header className={classes.header}>
+            <Link href="/" className={classes.anchor}>Index</Link>
+            <Link href="/about" className={classes.anchor}>about</Link>
+        </header>
+    );
+}
