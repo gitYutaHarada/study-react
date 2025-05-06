a78670f9f696aba1dbfbfdb230c0e1c388dbbf21
haradayuuta <mayutokuyuta@gmail.com>
2025-05-06 16:39:44 +0900

    🎨 拡張子の変更、fanctionの使用法の確認

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,26 @@
+'use client';
+
 import { Footer } from "@/components/Footer";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
 
+function handleClick (e) {
+  console.log(e.target.href);
+  e.preventDefault();
+};
+
 export default function Home() {
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
-      <Main page="index"/>
+      <a
+        href="/about"
+        onClick={handleClick}
+      >
+        ボタン
+      </a>
+      <Main page="index" />
       <Footer />
     </div>
   );
