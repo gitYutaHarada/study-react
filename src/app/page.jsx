14a356daca9d8ef90187d1f88ab01c7202e04b06
haradayuuta <mayutokuyuta@gmail.com>
2025-05-07 16:54:29 +0900

    ✨ カウンターを設置してuseStateでカウントアップする

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,19 +3,18 @@
 import { Footer } from "@/components/Footer";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
-import { useCallback, useEffect } from "react";
+import { useEffect, useState } from "react";
 
 
 
 export default function Home() {
-  const foo = 1;
+  const [count, setCount] = useState(1);
 
-  const handleClick = useCallback((e) => {
-    console.log(e.target.href);
-    //ブラウザがもともと行う操作をキャンセルする
-    e.preventDefault();
-    alert(foo);
-  }, []);
+  const handleClick = (e) => {
+
+    setCount(count => count + 1);
+    setCount(count => count + 1);
+  };
 
   useEffect( () => {
     document.body.style.backgroundColor = "lightblue";
@@ -28,12 +27,12 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
-      <a
-        href="/about"
+      <h1>{count}</h1>
+      <button
         onClick={handleClick}
       >
         ボタン
-      </a>
+      </button>
       <Main page="index" />
       <Footer />
     </div>
