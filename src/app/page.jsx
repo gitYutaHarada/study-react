1aa3c80c3808f19b8d89d8a115336aa85d0c15e4
haradayuuta <mayutokuyuta@gmail.com>
2025-05-07 18:01:34 +0900

    ✨ useStateで文字列と真偽値について勉強

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -9,30 +9,47 @@ import { useCallback, useEffect, useState } from "react";
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
-  const handleClick = useCallback( () => {
-    if(count < 10){
+  const handleClick = useCallback(() => {
+    if (count < 10) {
       setCount(count => count + 1);
     }
   }, [count]);
 
-  useEffect( () => {
+  const handleDisplay = useCallback (() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
+
+  const handleChange = useCallback ((e) => {
+    setText(e.target.value);
+  }, []);
+
+  useEffect(() => {
     document.body.style.backgroundColor = "lightblue";
-    
+
     return () => {
       document.body.style.backgroundColor = "";
     }
-  },[]);
-  
+  }, []);
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
       <button
         onClick={handleClick}
       >
         ボタン
       </button>
+      <button onClick={handleDisplay}
+      >
+        {isShow ? "非表示" : "表示"}
+      </button>
+      <input type="text" value={text} onChange={handleChange}
+      // className="border border-gray-400 rounded px-3 py-2 bg-white"
+      />
       <Main page="index" />
       <Footer />
     </div>
