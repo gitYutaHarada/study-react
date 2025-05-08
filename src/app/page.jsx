2fe63d73fe7861b46349446102ad1aed289ea803
haradayuuta <mayutokuyuta@gmail.com>
2025-05-08 14:50:04 +0900

    ✨ useStateで配列を使用した

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,16 +1,15 @@
-'use client';
+"use client";
 
 import { Footer } from "@/components/Footer";
 import { Main } from "@/components/Main";
 import { Header } from "@/components/Header";
 import { useCallback, useEffect, useState } from "react";
 
-
-
 export default function Home() {
   const [count, setCount] = useState(1);
   const [text, setText] = useState("");
   const [isShow, setIsShow] = useState(true);
+  const [array, setArray] = useState([]);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
@@ -22,34 +21,46 @@ export default function Home() {
     setIsShow((prevIsShow) => !prevIsShow);
   }, []);
 
-  const handleChange = useCallback ((e) => {
+  const handleChange = useCallback((e) => {
     setText(e.target.value);
   }, []);
 
+  const handleAdd = useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some((item) => item === text)) {
+        alert("同じ要素があります");
+        return newArray;
+      }
+      return [...prevArray, text];
+    });
+  }, [text]);
+
   useEffect(() => {
-    document.body.style.backgroundColor = "lightblue";
+    document.body.style.backgroundColor = "black";
 
     return () => {
       document.body.style.backgroundColor = "";
-    }
+    };
   }, []);
 
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <Header />
       {isShow ? <h1>{count}</h1> : null}
-      <button
-        onClick={handleClick}
-      >
-        ボタン
-      </button>
-      <button onClick={handleDisplay}
-      >
-        {isShow ? "非表示" : "表示"}
-      </button>
-      <input type="text" value={text} onChange={handleChange}
-      // className="border border-gray-400 rounded px-3 py-2 bg-white"
+      <button onClick={handleClick}>ボタン</button>
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+        className="border border-gray-400 rounded px-3 py-2 bg-white"
       />
+      <button onClick={handleAdd}>追加</button>
+      <ul>
+        {array.map((item) => {
+          return <li key={item}>{item}</li>;
+        })}
+      </ul>
       <Main page="index" />
       <Footer />
     </div>
