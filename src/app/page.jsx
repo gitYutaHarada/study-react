669fc1e148a688737031f3531f549cb9df621606
haradayuuta <mayutokuyuta@gmail.com>
2025-05-08 14:49:17 +0900

    ♻️ 状態を変更するときにprevを付けて名前を分かりやすくした

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,12 +14,12 @@ export default function Home() {
 
   const handleClick = useCallback(() => {
     if (count < 10) {
-      setCount(count => count + 1);
+      setCount((prevCount) => prevCount + 1);
     }
   }, [count]);
 
-  const handleDisplay = useCallback (() => {
-    setIsShow((isShow) => !isShow);
+  const handleDisplay = useCallback(() => {
+    setIsShow((prevIsShow) => !prevIsShow);
   }, []);
 
   const handleChange = useCallback ((e) => {
